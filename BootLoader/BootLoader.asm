e556ff3edb0afdf1d985b1530ce641e349844937
YeongsikLee <leejk9592@naver.com>
2019-06-16 14:46:17 +0900

    Bootloader v0.0.0

--- BootLoader/BootLoader.asm
+++ BootLoader/BootLoader.asm
@@ -1,73 +1,60 @@
-[ORG 0x00]			; Start Address is 0x00
-[BITS 16]			; Follow Codes are executed with 16 bit mode
+[ORG 0x00]
+[BITS 16]
 
-SECTION .text		; text Section
+SECTION .text
+					; Value of Segment Register will be translated by multiplying 16(= << 4), See 93 Page
+jmp 0x07C0:START	; Copy "0x07C0" To CS Segment Reg
+					; Copy "Address of START Label" to IP Reg
 
-mov ax, 0xB800		; copy 0xB800 to AX Reg
-mov ds, ax			; copy Value of AX Reg to DS Segment Reg
+START:				; global function
+	mov ax, 0x07C0	; Address of Bootloader
+	mov ds, ax		; Set Address of Bootloader to DS Segment Register
+	mov ax, 0xB800	; Address of Video Memory
+	mov es, ax		; Set Address of Video Memory to ES Segment Register
 
-mov byte [0x00], 'B'
-mov byte [0x01], 0x4A
-mov byte [0x02], 'B'
-mov byte [0x03], 0x4A
-mov byte [0x04], 'i'
-mov byte [0x05], 0x4A
-mov byte [0x06], ' '
-mov byte [0x07], 0x4A
-mov byte [0x08], 'O'
-mov byte [0x09], 0x4A
-mov byte [0x0A], 'S'
-mov byte [0x0B], 0x4A
-mov byte [0x0C], ' '
-mov byte [0x0D], 0x4A
-mov byte [0x0E], 'B'
-mov byte [0x0F], 0x4A
-mov byte [0x10], 'o'
-mov byte [0x11], 0x4A
-mov byte [0x12], 'o'
-mov byte [0x13], 0x4A
-mov byte [0x14], 't'
-mov byte [0x15], 0x4A
-mov byte [0x16], 'l'
-mov byte [0x17], 0x4A
-mov byte [0x18], 'o'
-mov byte [0x19], 0x4A
-mov byte [0x1A], 'a'
-mov byte [0x1B], 0x4A
-mov byte [0x1C], 'd'
-mov byte [0x1D], 0x4A
-mov byte [0x1E], 'e'
-mov byte [0x1F], 0x4A
-mov byte [0x20], 'r'
-mov byte [0x21], 0x4A
-mov byte [0x22], ' '
-mov byte [0x23], 0x4A
-mov byte [0x24], 'S'
-mov byte [0x25], 0x4A
-mov byte [0x26], 't'
-mov byte [0x27], 0x4A
-mov byte [0x28], 'a'
-mov byte [0x29], 0x4A
-mov byte [0x2A], 'r'
-mov byte [0x2B], 0x4A
-mov byte [0x2C], 't'
-mov byte [0x2D], 0x4A
-mov byte [0x2E], '!'
-mov byte [0x2F], 0x4A
-mov byte [0x30], '!'
-mov byte [0x31], 0x4A
-mov byte [0x32], '!'
+	mov si, 0		; SI Register(String source index Register)
 
-jmp $				; Infinite Loop on current location
+.SCREENCLEARLOOP:					; .FUNCTION is local Function
+	mov byte [ es: si ], 0			; Write 0x00 to clear Video Memory
+	mov byte [ es: si + 1 ], 0x0A	; Write 0x0A to set Attr to printing green char, black background
 
-times 510 - ($ - $$) db 0x00	; $: Address of Current Line
-								; $$: Start Address of Current Section(text)
-								; $ - $$: Offset From Current Section
-								; 510 - ( $ - $$ ): From Current Address To Address 510
-								; db 0x00: Declare 1 Byte and Assign Value "0"
-								; time: Loop Statement
-								; From Current Address To Address 510, Write 0
+	add si, 2						; Move next char
+	cmp si, 80 * 25 * 2				; Compare si and (80 * 25 * 2) to check it finished
 
-db 0x55							; Declare 1 Byte and Assign Value "0x55"
-db 0xAA							; Declare 1 Byte and Assign Value "0xAA"
-								; By Writing 0x55 and 0xAA At Address 511 and 512, Mark this sector as Boot Sector
+	jl .SCREENCLEARLOOP				; if not, print next
+
+	mov si, 0						; Init SI Reg
+	mov di, 0						; Init DI Reg
+
+.MESSAGELOOP:						; Loop to print message
+	mov cl, byte [ si + MESSAGE1 ]	; MESSAGE1 주소에서 SI 레지스터 값 만큼 더한 위치의 문자를 CL 레지스터에 복사
+									; 문자열은 1 바이트면 충분하므로, CX 레지스터 하위 1바이트만 사용
+
+	cmp cl, 0				; Compare copied char and 0x0
+	je .MESSAGEEND			; if char is 0x0, then goto .MESSAGEEND to finish
+
+	mov byte [ es: di ], cl	; if not, mov value of CL Reg to (value of ES) + DI
+	
+	add si, 1				; SI 레지스터에 1을 더해 다음 문자열로 이동
+	add di, 2				; DI 레지스터에 2를 더해, 비디오 메모리 다음 문자 출력 위치로 이동
+							; 비디오 메모리는 (문자, 속성) 쌍으로 구성되므로, 속성 없이 문자만 설정하기 위해서는 2씩 이동
+	
+	jmp .MESSAGELOOP		; 다음 문자를 출력하기 위해 이동
+	
+.MESSAGEEND:
+	jmp	$					; 현재 위치에서 무한루프 수행
+
+MESSAGE1:
+	db 'BBI OS Boot Loader Start~!!', 0	; 마지막 값을 0으로 설정해 .MESSAGELOOP에서 문자열이 종료됐음을 인지할 수 있도록 만듦
+	
+times 510 - ($ - $$) db 0x00	; $					현재 Line의 Address
+								; $$				현재 Section(.text)의 Src Address
+								; $ - $$			현재 Section을 기준으로 하는 Offset
+								; 510 - ($ - $$)	현재부터 Adress 510 까지
+								; db 0x00			1바이트를 선언하고 값은 0
+								; time				반복 수행
+								; 현 위치에서 510 번지까지 0x00 쓰기
+
+db 0x55							; 511번지에 0x55를 쓰기
+db 0xAA							; 512번지에 0xAA를 쓰기
+								; 부트 섹터임을 표기
---
+++ qemu.sh
@@ -0,0 +1,1 @@
+qemu-system-x86_64 -L . -m 64 -fda Disk.img -rtc base=localtime  -M pc
