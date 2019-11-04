3299c80b01502502ed68bab23bd633075ca2ac3c
Sikurity <leejk9592@naver.com>
2019-11-04 23:57:17 +0900

    Modify AssemblyUtility

--- Kernel64/src/AssemblyUtil.h
+++ Kernel64/src/AssemblyUtil.h
@@ -5,5 +5,8 @@
 
 BYTE kInPortByte(WORD port);
 BYTE kOutPortByte(WORD port, BYTE data);
+void kLoadGDTR(QWORD pGDTR);
+void kLoadTR(WORD offset);
+void kLoadIDTR(QWORD pIDTR);
 
 #endif  // __ASSEMBLYUTIL_H__
--- Kernel64/src/AssemblyUtility.asm
+++ Kernel64/src/AssemblyUtility.asm
@@ -3,7 +3,13 @@
 SECTION .text       ; text 섹션 정의
 
 ; C 언어에서 호출 가능하도록 함수 Export
-global kInPortByte, kOutPortByte
+global kInPortByte
+global kOutPortByte
+global kLoadGDTR
+global kLoadTR
+global kLoadIDTR
+
+SECTION .text
 
 ; Port로 부터 1 Byte 읽기
 ; PARAM: WORD portNum
@@ -31,3 +37,24 @@ kOutPortByte:
     pop rax         ; 함수에서 사용이 끝난 레지스터 복원
     pop rdx         ; 함수에서 사용이 끝난 레지스터 복원
     ret             ; 함수 호출 후, 다음 실행 코드 위치로 복귀
+
+; GDTR 레지스터에 GDT 테이블 설정
+; PARAM: QWORD pGDT
+kLoadGDTR:
+    lgdt [rdi]  ; Param 0(Address of GDTR)를 프로세서에 로드
+                ; GDT 테이블 설정
+    ret
+
+; TR 레지스터에 TS 세그먼트 디스크립터 설정
+; PARAM: WORD offset
+kLoadTR:
+    ltr di      ; Param 0(offset of TS 세그먼트를 프로세서에 설정
+                ; Load TS(Task State) Segment
+    ret
+
+; IDTR 레지스터에 IDT 테이블 설정
+; PARAM: QWORD pIDTR
+kLoadIDTR:
+    lidt [rdi]  ; Param 0(Address of IDTR)를 프로세서에 로드
+                ; IDT 테이블 설정
+    ret
