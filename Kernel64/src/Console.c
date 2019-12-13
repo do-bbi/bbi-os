da34176c0c3c397df29dae31334df0b540ed765e
Sikurity <leejk9592@naver.com>
2019-12-14 00:44:31 +0900

    Add Util Functions for Console

--- Kernel64/src/Console.c
+++ Kernel64/src/Console.c
@@ -1,11 +1,15 @@
 #include <stdarg.h>
+#include "Utility.h"
 #include "Console.h"
 #include "Keyboard.h"
 
 CONSOLEMANAGER gConsoleManager = {0, };
 
 void kInitializeConsole(int x, int y) {
-    kMemSet(gConsoleManager, 0, sizeof(gConsoleManager));
+    kMemSet(&gConsoleManager, 0, sizeof(gConsoleManager));
+
+    // Set Position of Cursor 
+    kSetCursor(x, y);
 }
 
 void kSetCursor(int x, int y) {
@@ -33,8 +37,107 @@ void kGetCursor(int *pX, int *pY) {
     *pY = gConsoleManager.currentPrintOffset % CONSOLE_WIDTH;
 }
 
-void kPrintf(const char *pFormatStr, ...);
-int kConsolePrintString(const char *pBuf);
-void kClearScreen(void);
-BYTE kGetCh(void);
-void kPrintStringXY(int x, int y, const char *pStr);
+void kPrintf(const char *pFormatStr, ...) {
+    va_list ap;
+    
+    char pBuf[1024];
+    int nextPrintOffset;
+
+    // Use Variable Parameters
+
+    // #define va_start(AP, LASTARG) (AP = ((char *) &(LASTARG) + __va_rounded_size (LASTARG)))
+    va_start(ap, pFormatStr);
+    kVSPrintf(pBuf, pFormatStr, ap);
+
+    // #define va_end(ap) ap = (va_list)NULL
+    va_end(ap);
+
+    // Print Format String to console
+    nextPrintOffset = kConsolePrintString(pBuf);
+
+    // Update Cursor Position
+    kSetCursor(nextPrintOffset % CONSOLE_WIDTH, nextPrintOffset / CONSOLE_WIDTH); 
+ }
+
+int kConsolePrintString(const char *pBuf) {
+    CHARACTER *pScreen = (CHARACTER *)CONSOLE_VIDEO_MEM_ADDR;
+
+    int i, j, len;
+    int printOffset;
+
+    printOffset = gConsoleManager.currentPrintOffset;
+
+    // Print String
+    len = kStrLen(pBuf);
+    for(i = 0; i < len; ++i) {
+        if(pBuf[i] == '\n') // 줄 바꾸기 + 현재 라인의 남은 문자열을 다음 라인으로 옮김
+            printOffset += (CONSOLE_WIDTH - (printOffset % CONSOLE_WIDTH));
+        else if(pBuf[i] == '\t')
+            printOffset += (8 - (printOffset % 8));
+        else {
+            pScreen[printOffset].bCharacter = pBuf[i];
+            pScreen[printOffset].bAttribute = CONSOLE_DEFAULT_TEXTCOLOR;
+            printOffset++;
+        }
+        
+        if(printOffset >= (CONSOLE_HEIGHT * CONSOLE_WIDTH)) {
+            // 가장 윗줄을 제외한 나머지를 한 줄 위로 복사
+            kMemCpy(CONSOLE_VIDEO_MEM_ADDR, 
+                CONSOLE_VIDEO_MEM_ADDR + CONSOLE_WIDTH * sizeof(CHARACTER),
+                (CONSOLE_HEIGHT - 1) * CONSOLE_WIDTH * sizeof(CHARACTER));
+
+            // 가장 마지막 라인은 공백으로 채움
+            for(j = (CONSOLE_HEIGHT - 1) * (CONSOLE_WIDTH); j < (CONSOLE_HEIGHT * CONSOLE_WIDTH); ++j) {
+                pScreen[j].bCharacter = ' ';
+                pScreen[j].bAttribute = CONSOLE_DEFAULT_TEXTCOLOR;
+            }
+
+            // 출력할 위치를 가장 아래쪽 라인의 처음으로 설정
+            printOffset = (CONSOLE_HEIGHT - 1) * CONSOLE_WIDTH;
+        }
+    }
+
+    return printOffset;
+}
+
+void kClearScreen(void) {
+    CHARACTER *pScreen = (CHARACTER *)CONSOLE_VIDEO_MEM_ADDR;
+    int i;
+
+    for(int i = 0; i < CONSOLE_WIDTH * CONSOLE_HEIGHT; ++i) {
+        pScreen[i].bCharacter = ' ';
+        pScreen[i].bAttribute = CONSOLE_DEFAULT_TEXTCOLOR;
+    }
+
+    // 커서를 화면 상단으로 이동
+    kSetCursor(0, 0);
+}
+
+BYTE kGetCh(void) {
+    KEYDATA keyData;
+
+    // Wait until key pressed
+    while(TRUE) {
+        // Wait until key pushed into queue
+        while(kGetKeyFromKeyQueue(&keyData) == FALSE);
+
+        // 키가 눌렸다는 데이터가 수신되면 ASCII 코드 반환
+        if(keyData.flags & KEY_FLAGS_DOWN)
+            return keyData.asciiCode;
+    }
+}
+
+// Print String at Position(X, Y)
+void kPrintStringXY(int x, int y, const char *pStr) {
+    CHARACTER *pScreen = (CHARACTER *)CONSOLE_VIDEO_MEM_ADDR;
+    int i;
+
+    // Calculate position to print string
+    pScreen += y * CONSOLE_WIDTH + x;
+
+    for(i = 0; pStr[i]; ++i) {
+        pScreen[i].bCharacter = pStr[i];
+        pScreen[i].bAttribute = CONSOLE_DEFAULT_TEXTCOLOR;
+
+    }
+}
--- Kernel64/src/Utility.c
+++ Kernel64/src/Utility.c
@@ -1,5 +1,6 @@
 #include "Utility.h"
 #include "AssemblyUtil.h"
+#include <stdarg.h>
 
 // 메모리를 특정 값으로 채움
 void kMemSet(void *pDst, BYTE data, int size) {
@@ -31,6 +32,103 @@ int kMemCmp(const void *pDst, const void *pSrc, int size) {
     }
 
     return 0;
+}
+
+int kStrLen(const char *pStr) {
+    int len = 0;
+    while(pStr[len])
+        ++len;
+
+    return len;
+}
+
+// Total sizeof Main Memory (Unit = MBs)
+static int gTotalSizeofRAM = 0;
+
+// Check from 64MBs to end, Must call at booting once
+void kCheckTotalSizeofRAM(void) {
+    DWORD *pCurrentAddr;
+    DWORD previousValue;
+
+    // Check from 0x4000000(64MB) in 4MB increments
+    pCurrentAddr = (DWORD *)0x400000;
+    while(TRUE) {
+        previousValue = *pCurrentAddr;  // Save value of previous memory
+
+        // Write 0x12345678 & Check it is 0x12345678
+        *pCurrentAddr = 0x12345678;
+        if(*pCurrentAddr != 0x12345678)
+            break;
+
+        // Restore previous value
+        *pCurrentAddr = previousValue;
+
+        // Increment 4MBs
+        pCurrentAddr += (0x400000 / 4);
+    }
+
+    // Calculate Total sizeof RAM by dividing 1MB
+    gTotalSizeofRAM = (QWORD) pCurrentAddr / 0x100000;
+}
+
+// Return Total sizeof RAM
+QWORD kGetTotalRAMSize(void) {
+    return gTotalSizeofRAM;
+}
+
+// Implement atoi()
+long kAtoI(const char *pBuf, int radix) {
+    long ret;
+
+    switch(radix) {
+        case 16:
+            ret = kHexStringToQword(pBuf);
+            break;
+        
+        case 10:
+        default:
+            ret = kDecStringToLong(pBuf);
+            break;
+    }
+
+    return ret;
+}
+
+// Convert Hexa String to QWORD
+QWORD kHexStringToQword(const char *pBuf) {
+    // Please Implement Me
+}
+
+// Convert Decimal String to QWORD
+long kDecStringToLong(const char *pBuf) {
+    // Please Implement Me
+}
+
+int kItoA(long value, char *pBuf, int radix) {
+    // Please Implement Me
+}
+
+// Convert QWORD to Hexa String
+QWORD kHexToString(QWORD value, char *pBuf) {
+    // Please Implement Me
+}
+
+// Convert QWORD to Decimal String
+long kDecToString(long value, char *pBuf) {
+    // Please Implement Me
+}
+
+// Reverse String
+void kReverseString(char *pBuf) {
+    // Please Implement Me
+}
+
+int kSPrintf(char *pBuf, const char *pFormatStr, ...) {
+    // Please Implement Me
+}
+
+int kVPrintf(char *pBuf, const char *pFormatStr, va_list ap) {
+    // Please Implement Me
 }
 
 // RFLAGS 레지스터의 인터럽트 플래그를 변경하고, 이전 인터럽트 플래그이 상태를 반환
