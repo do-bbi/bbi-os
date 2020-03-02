c6582c3a4d003810b0ad7c35462f84b197d1a525
Sikurity <leejk9592@naver.com>
2020-03-02 23:44:39 +0900

    Add List Structure For Managing Task

---
+++ Kernel64/include/List.h
@@ -0,0 +1,46 @@
+#ifndef __LIST_H__
+#define __LIST_H__
+
+#include "Types.h"
+
+// Structure
+#pragma pack(push, 1)
+
+// Must be in front of the data
+typedef struct kListLinkStruct {
+    void *pNext;    // Address of Next Data
+    QWORD id;       // Unique ID
+} LISTLINK;
+
+/*
+struct kListItemExampleStruct {
+    LISTLINK listLink;
+
+    int i;
+    char c;
+};
+*/
+
+// Data Structure to manage list
+typedef strcut kListManagerStruct {
+    int itemCount;
+
+    void *pHead;    // Address of First Item
+    void *pTail;    // Address of Last Item
+} LIST;
+
+#pragma pack(pop)
+
+// Function
+void kInitializeList(LIST *pList);
+int kGetListCount(const LIST *pList);
+void kAddListToTail(LIST *pList, void *pItems);
+void kAddListToHead(LIST *pList, void *pItems);
+void *kRemoveListFromHead(LIST *pList);
+void *kRemoveListFromTail(LIST *pList);
+void *kFindList(const LIST *pList, QWORD id);
+void *kGetHeadFromList(const LIST *pList);
+void *kGetTailFromList(const LIST *pList);
+void *kGetNextFromList(const LIST *pList, void *pCurItem);
+
+#endif  // __LIST_H__
