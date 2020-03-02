d37ff8728fa763caa391a95eecb56a1457725447
Sikurity <leejk9592@naver.com>
2020-03-02 23:24:47 +0900

    Remove Headers in src directory

--- Kernel32/src/ModeSwitch.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MODESWITCH_H__
-#define __MODESWITCH_H__
-
-#include "Types.h"
-
-void kReadCPUID(DWORD eax_val, DWORD *pEAX, DWORD *pEBX, DWORD *pECX, DWORD *pEDX);
-void kSwitchAndExecute64bitKernel(void);
-
-#endif  // __MODESWITCH_H__
