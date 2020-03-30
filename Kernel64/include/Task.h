450a2fe89ff709091d1abdcf9319d2c61fe6fca8
Sikurity <leejk9592@naver.com>
2020-03-30 23:34:27 +0900

    Implement Task Switching

--- Kernel64/include/Task.h
+++ Kernel64/include/Task.h
@@ -2,6 +2,7 @@
 #define __TASK_H__
 
 #include "Types.h"
+#include "List.h"
 
 // Macro
 // SS, RSP, RFLAGS, CS, RIP + ISR에서 저장하는 19개 레지스터
@@ -35,6 +36,20 @@
 #define TASK_RSP_OFFSET     (22)
 #define TASK_SS_OFFSET      (23)
 
+// Address For Task Pool
+#define TASK_TCB_POOL_ADDR      (0x800000)
+#define TASK_MAX_COUNT          (1024)
+
+// Address & Size For Stack Pool
+#define TASK_STACK_POOL_ADDR    (TASK_TCB_POOL_ADDR + sizeof(TCB) * TASK_MAX_COUNT)
+#define TASK_STACK_SIZE         (8192)
+
+// Invalid Task ID
+#define TASK_INVALID_ID         (0xFFFFFFFFFFFFFFFF)
+
+// Processor Time that Task Can Use Maximum
+#define TASK_PROCESSOR_TIME     (5) // ms
+
 // Struct
 #pragma pack(push, 1)
 
@@ -43,6 +58,7 @@ typedef struct kContextStruct {
 } CONTEXT;
 
 typedef struct kTaskControlBlockStruct {
+    LISTLINK link;
     CONTEXT context;
 
     QWORD id;
@@ -52,9 +68,47 @@ typedef struct kTaskControlBlockStruct {
     QWORD stackSize;
 } TCB;
 
+// Struct For Managing TCB Pool
+typedef struct kTCBPoolManagerStruct {
+    // Info for Task Pool
+    TCB *pStartAddr;
+    int maxCount;
+    int useCount;
+
+    // Count of allocated TCB
+    int allocCount;
+} TCBPOOLMANAGER;
+
+// Struct For Managing Scheduler
+typedef struct kSchedulerStruct {
+    // TCB handle of Running Task
+    TCB *pRunningTask;
+
+    // Time that Task in running can use
+    int processorTime;
+
+    // Tasks ready to run
+    LIST readyList;
+} SCHEDULER;
+
 #pragma pack(pop)
 
-// Function
+// Function For Task & Task Pool
+void kInitializeTCBPool(void);
+TCB *kAllocateTCB(void);
+void kFreeTCB(QWORD id);
+TCB *kCreateTask(QWORD flags, QWORD entryPointAddr);
 void kSetUpTask(TCB *pTCB, QWORD id, QWORD flags, QWORD entryPointerAddr, void *pStackAddr, QWORD stackSize);
+
+// Function For Scheduler
+void kInitializeScheduler(void);
+void kSetRunningTask(TCB *pTask);
+TCB *kGetRunningTask(void);
+TCB *kGetNextTaskToRun(void);
+void kAddTaskToReadyList(TCB *pTask);
+void kSchedule(void);
+BOOL kScheduleInInterrupt(void);
+void kDecreaseProcessorTime(void);
+BOOL kIsProcessorTimeExpired(void);
 
 #endif  // __TASK_H__
--- Kernel64/src/Task.c
+++ Kernel64/src/Task.c
@@ -1,5 +1,81 @@
 #include "Task.h"
 #include "Descriptor.h"
+#include "Utility.h"
+
+// Structure for Scheduling
+static SCHEDULER        gScheduler;
+static TCBPOOLMANAGER   gTCBPoolManager;
+
+// Initialize Task Pool
+void kInitializeTCBPool(void) {
+    int i;
+
+    kMemSet(&gTCBPoolManager, 0, sizeof(gTCBPoolManager));
+
+    gTCBPoolManager.pStartAddr = (TCB *)TASK_TCB_POOL_ADDR;
+    kMemSet(TASK_TCB_POOL_ADDR, 0, sizeof(TCB) * TASK_MAX_COUNT);
+
+    for(i = 0; i < TASK_MAX_COUNT; ++i)
+        gTCBPoolManager.pStartAddr[i].link.id = i;
+
+    gTCBPoolManager.maxCount = TASK_MAX_COUNT;
+    gTCBPoolManager.allocCount = 1;
+}
+
+// Allocate TCB
+TCB *kAllocateTCB(void) {
+    TCB *pEmptyTCB;
+    int i;
+
+    if(gTCBPoolManager.useCount == gTCBPoolManager.maxCount)
+        return NULL;
+
+    for(i = 0; i < gTCBPoolManager.maxCount; ++i) {
+        // if id[32] == 0, then non-allocated 
+        if((gTCBPoolManager.pStartAddr[i].link.id >> 32) == 0) {
+            pEmptyTCB = g(gTCBPoolManager.pStartAddr[i]);
+            break;
+        }
+    }
+
+    pEmptyTCB->link.id = ((QWORD) gTCBPoolManager.allocCount << 32) | i;
+    gTCBPoolManager.useCount++;
+    gTCBPoolManager.allocCount++;
+
+    if(gTCBPoolManager.allocCount == 0)
+        gTCBPoolManager.allocCount = 1;
+
+    return pEmptyTCB;
+}
+
+// Release TCB
+void kFreeTCB(QWORD id) {
+    int i;
+
+    // Lower bits[31:0] of TASK ID is index
+    i = id & 0xFFFFFFFF;
+
+    // Init TCB & Set ID
+    kMemSet(&gTCBPoolManager.pStartAddr[i].context, 0, sizeof(CONTEXT));
+    gTCBPoolManager.pStartAddr[i].link.id = i;
+
+    gTCBPoolManager.useCount--;
+}
+
+// Create Task
+TCB *kCreateTask(QWORD flags, QWORD entryPointAddr) {
+    TCB *pTask;
+    void *pStackAddr;
+
+    pTask = kAllocateTCB();
+    if(pTask == NULL)
+        return NULL;
+
+    pStackAddr = (void *)(TASK_STACK_POOL_ADDR + (TASK_STACK_SIZE * (pTask->link.id & 0xFFFFFFFF)));
+    kAddTaskToReadyList(pTask);
+
+    return pTask;
+}
 
 // Set TCB using Function Paramter
 void kSetUpTask(TCB *pTCB, QWORD id, QWORD flags, QWORD entryPointAddr, void *pStackAddr, QWORD stackSize) {
@@ -29,4 +105,104 @@ void kSetUpTask(TCB *pTCB, QWORD id, QWORD flags, QWORD entryPointAddr, void *pS
     pTCB->pStackAddr = pStackAddr;
     pTCB->stackSize = stackSize;
     pTCB->flags = flags;
+}
+
+// Function For Scheduler
+// Initialize Scheduler
+void kInitializeScheduler(void) {
+    kInitializeTCBPool();
+
+    kInitializeList(&(gScheduler.readyList));
+
+    gScheduler.pRunningTask = kAllocateTCB();
+}
+
+// Set Task which is Running
+void kSetRunningTask(TCB *pTask) {
+    gScheduler.pRunningTask = pTask;
+}
+
+// Get Task which is Running
+TCB *kGetRunningTask(void) {
+    return gScheduler.pRunningTask;
+}
+
+// Get Next Task from Task List
+TCB *kGetNextTaskToRun(void) {
+    if(kGetListCount(&(gScheduler.readyList)) == 0)
+        return NULL;
+
+    return (TCB *)kRemoveListFromHead(&(gScheduler.readyList));
+}
+
+// Add Task to Tail of Task List
+void kAddTaskToReadyList(TCB *pTask) {
+    kAddListToTail(&gScheduler.readyList, pTask);
+}
+
+// Context Switching - Don't call this function at interrupt/exception handler
+void kSchedule(void) {
+    TCB *pRunningTask, *pNextTask;
+    BOOL bPrevFlag;
+
+    // Check is there any task to run
+    if(kGetListCount(&gScheduler.readyList) == 0)
+        return;
+    
+    // Deactivate Interrupt to block context switching by interrupts
+    bPrevFlag = kSetInterruptFlag(FALSE);
+
+    // Get Next Task to run
+    pNextTask = kGetNextTaskToRun();
+    if(pNextTask != NULL) {
+        pRunningTask = gScheduler.pRunningTask;
+        kAddTaskToReadyList(pRunningTask);
+
+        // Update Available Processor time
+        gScheduler.processorTime = TASK_PROCESSOR_TIME; // 5 tick(ms)
+
+        // Set "next task" as the Currently Running Task & Context Switching
+        gScheduler.pRunningTask = pNextTask;
+        kSwitchContext(&pRunningTask->context, &pNextTask->context);   
+    }
+
+    kSetInterruptFlag(bPrevFlag);
+}
+
+// Schedule only For Interrupt/Exception Handler
+BOOL kScheduleInInterrupt(void) {
+    TCB *pRunningTask, *pNextTask;
+    char *pContextAddress;
+
+    // Get Next Task to run
+    pNextTask = kGetNextTaskToRun();
+    if(pNextTask == NULL)
+        return FALSE;
+
+    // Context Switching to Interrupt/Exception Handler
+    pContextAddress = (char *)IST_BASE_ADDR + IST_SIZE - sizeof(CONTEXT);
+
+    pRunningTask = gScheduler.pRunningTask;
+    kMemCpy(&pRunningTask->context, pContextAddress, sizeof(CONTEXT));
+    kAddTaskToReadyList(pRunningTask);
+
+    // Set "next task" as the Currently Running Task & Context Switching
+    gScheduler.pRunningTask = pNextTask;
+    kMemCpy(pContextAddress, &pNextTask->context, sizeof(CONTEXT));
+
+    // Update Available Processor time
+    gScheduler.processorTime = TASK_PROCESSOR_TIME; // 5 tick(ms)
+
+    return TRUE;
+}
+
+// Decrease Processor Time
+void kDecreaseProcessorTime(void) {
+    if(gScheduler.processorTime > 0)
+        gScheduler.processorTime--;
+}
+
+// Check Processor Time is Expired
+BOOL kIsProcessorTimeExpired(void) {
+    return (gScheduler.processorTime <= 0);
 }
