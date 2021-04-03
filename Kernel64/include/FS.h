988f627b7f608261583e08620b017ac5176da59d
Sikurity <leejk9592@naver.com>
2021-04-03 18:55:12 +0900

    Update C standard File System Functions Interface
    Signed-off-by: Sikurity <leejk9592@naver.com>

--- Kernel64/include/FS.h
+++ Kernel64/include/FS.h
@@ -2,6 +2,7 @@
 #define __FS_H__
 
 #include "Types.h"
+#include "Task.h"
 #include "Sync.h"
 #include "HDD.h"
 
@@ -21,13 +22,47 @@
 #define FS_CLUTSER_SIZE             (FS_SECTORS_PER_CLUSTER * FS_SECTOR_SIZE)
 // Maximum count of directories
 #define FS_MAX_DIR_ENTRY_COUNT      (FS_CLUTSER_SIZE / sizeof(DIR_ENTRY))
+// Maximum count of Handle
+#define FS_HANDLE_MAX_COUNT         (TASK_MAX_COUNT * 3)
 
 // Maximum length of File Name
 #define FS_MAX_FILENAME_LENGTH      (24)
 
+// Handle Type
+#define FS_TYPE_FREE                (0)
+#define FS_TYPE_FILE                (1)
+#define FS_TYPE_DIR                 (2)
+
+// Seek Option Flag
+#define FS_SEEK_SET                 (0)
+#define FS_SEEK_CUR                 (1)
+#define FS_SEEK_END                 (2)
+
 typedef BOOL (*fReadHDDInfo)(BOOL isPrimary, BOOL isMaster, HDD_INFO *pHDDInfo);
 typedef BOOL (*fReadHDDSector)(BOOL isPrimary, BOOL isMaster, DWORD lba, int sectorCnt, char *pBuf);
 typedef BOOL (*fWriteHDDSector)(BOOL isPrimary, BOOL isMaster, DWORD lba, int sectorCnt, char *pBuf);
+
+// C Standard API -> BBI OS Internal FS API
+#define fopen       kOpenFile
+#define fread       kReadFile
+#define fwrite      kWriteFile
+#define fseek       kSeekFile
+#define fclose      kCloseFile
+#define remove      kRemoveFile
+
+#define opendir     kOpenDir
+#define readdir     kReadDir
+#define rewinddir   kRewindDir
+#define closedir    kCloseDir
+
+// C Standard Type -> BBI OS FS Type
+#define SEEK_SET    FS_SEEK_SET
+#define SEEK_CUR    FS_SEEK_CUR
+#define SEEK_END    FS_SEEK_END
+
+#define size_t      DWORD
+#define dirent      DIR_ENTRY
+#define d_name      filename
 
 // Struct
 #pragma pack(push, 1)
@@ -76,7 +111,31 @@ typedef struct kDirectoryEntryStruct {
     DWORD startClusterIdx;
 } DIR_ENTRY;
 
-#pragma pack(pop);
+// File handle
+typedef struct kFileHandleStruct {
+    int dirEntryOffset;
+    DWORD filesize;
+    DWORD startClusterIdx;
+    DWORD curClusterIdx;
+    DWORD prevClusterIdx;
+    DWORD curOffset;
+} FILE_HANDLE;
+
+// Directory Handle
+typedef struct kDirectoryHandleStruct {
+    DIR_ENTRY *pDirBuf;
+    int curOffset;
+} DIR_HANDLE;
+
+// File, Directory Information Struct
+typedef struct kFileDirectoryHandleStruct {
+    BYTE type;
+
+    union {
+        FILE_HANDLE fileHandle;
+        DIR_HANDLE  dirHandle;
+    };
+} FILE, DIR;
 
 // FS Manager
 typedef struct kFSManagerStruct {
@@ -96,25 +155,52 @@ typedef struct kFSManagerStruct {
 
     // FS mutex for sync
     MUTEX mutex;
+
+    // Handle pool address
+    FILE *pHandlePool
 } FS_MANAGER;
 
-// Functions
+#pragma pack(pop);
+
+// Low Level Functions
 BOOL kInitializeFS(void);
 BOOL kFormat(void);
 BOOL kMount(void);
 BOOL kGetHDDInfo(HDD_INFO *pInfo);
 
-BOOL kReadClusterLinkTable(DWORD offset, BYTE *pBuf);
-BOOL kWriteClusterLinkTable(DWORD offset, BYTE *pBuf);
-BOOL kReadCluster(DWORD offset, BYTE *pBuf);
-BOOL kWriteCluster(DWORD offset, BYTE *pBuf);
-DWORD kFindFreeCluster(void);
-BOOL kSetClusterLinkData(DWORD clusterIdx, DWORD data);
-BOOL kGetClusterLinkData(DWORD clusterIdx, DWORD *pData);
-int kFindFreeDirEntry(void);
-BOOL kSetDirEntryData(int idx, DIR_ENTRY *pEntry);
-BOOL kGetDirEntryData(int idx, DIR_ENTRY *pEntry);
-int kFindDirEntry(const char *pFilename, DIR_ENTRY *pEntry);
+static BOOL kReadClusterLinkTable(DWORD offset, BYTE *pBuf);
+static BOOL kWriteClusterLinkTable(DWORD offset, BYTE *pBuf);
+static BOOL kReadCluster(DWORD offset, BYTE *pBuf);
+static BOOL kWriteCluster(DWORD offset, BYTE *pBuf);
+static DWORD kFindFreeCluster(void);
+static BOOL kSetClusterLinkData(DWORD clusterIdx, DWORD data);
+static BOOL kGetClusterLinkData(DWORD clusterIdx, DWORD *pData);
+static int kFindFreeDirEntry(void);
+static BOOL kSetDirEntryData(int idx, DIR_ENTRY *pEntry);
+static BOOL kGetDirEntryData(int idx, DIR_ENTRY *pEntry);
+static int kFindDirEntry(const char *pFilename, DIR_ENTRY *pEntry);
 void kGetFSInfo(FS_MANAGER *pManager);
+
+// High Level Functions
+FILE* kOpenFile(const char *filename, const char *pMode);
+DWORD kReadFile(void *pBuf, DWORD size, DWORD cnt, FILE *pFile);
+DWORD kWriteFile(const void *pBuf, DWORD size, DWORD cnt, FILE *pFile);
+int kSeekFile(FILE *pFile, int offset, int origin);
+int kCloseFile(FILE *pFile);
+int kRemoveFile(const char *filename);
+
+DIR *kOpenDir(const char *dirname);
+DIR_ENTRY *kReadDir(DIR *pDir);
+void *kRewindDir(DIR *pDir);
+int kCloseDir(DIR *pDir);
+
+DIR *kWriteZero(const char *filename, DWORD cnt);
+DIR *kIsFileOpened(const DIR_ENTRY *pEntry);
+
+static void *kAllocFileDirHandle(void);
+static void kFreeFileDirectoryHandle(FILE *pFile);
+static BOOL kCreateFile(const char *filename, DIR_ENTRY *pEntry, int *pDirEntryIdx);
+static BOOL kFreeClusterUntilEnd(DWORD clusterIdx);
+static BOOL kUpdateDirEntry(FILE_HANDLE *pFileHandle);
 
 #endif  // __FS_H__
